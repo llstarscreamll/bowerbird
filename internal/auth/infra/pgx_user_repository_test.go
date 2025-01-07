2d47ded66b6aa1ce22db1f5badd4a3caba8ae225
Johan Alvarez <llstarscreamll@hotmail.com>
2025-01-06 20:49:54 -0500

    refactor: move pgx test helpers to another package

--- internal/auth/infra/pgx_user_repository_test.go
+++ internal/auth/infra/pgx_user_repository_test.go
@@ -2,17 +2,11 @@ package infra
 
 import (
 	"context"
-	"fmt"
 	"llstarscreamll/bowerbird/internal/auth/domain"
 	"llstarscreamll/bowerbird/internal/common/infra/postgresql"
-	"log"
-	"maps"
-	"slices"
-	"strings"
+	"llstarscreamll/bowerbird/internal/common/infra/postgresql/tests"
 	"testing"
 
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -50,76 +44,14 @@ func TestUpsert(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.testCase, func(t *testing.T) {
 			ctx := context.Background()
-			cleanUpTables(db, []string{"users"})
-			writeScenarioRows(db, "users", tc.scenarioRows)
+			tests.CleanUpTables(db, []string{"users"})
+			tests.WriteScenarioRows(db, "users", tc.scenarioRows)
 
 			repo := NewPgxUserRepository(db)
 			err := repo.Upsert(ctx, tc.user)
 
 			assert.Nil(t, err)
-			assertDatabaseHasRows(t, db, "users", tc.expectedRows)
+			tests.AssertDatabaseHasRows(t, db, "users", tc.expectedRows)
 		})
-	}
-}
-
-func cleanUpTables(db *pgxpool.Pool, tables []string) {
-	for _, t := range tables {
-		db.Exec(context.Background(), fmt.Sprintf("DELETE FROM %s", t))
-	}
-}
-
-func writeScenarioRows(db *pgxpool.Pool, tableName string, rows []map[string]any) {
-	for _, row := range rows {
-		var columns []string
-		var values []interface{}
-		var placeholders []string
-
-		for k, v := range row {
-			columns = append(columns, k)
-			placeholders = append(placeholders, fmt.Sprintf("$%d", len(values)+1))
-			values = append(values, v)
-		}
-
-		query := fmt.Sprintf(
-			"INSERT INTO %s (%s) VALUES (%s)",
-			tableName, strings.Join(columns, ", "), strings.Join(placeholders, ", "),
-		)
-
-		_, err := db.Exec(
-			context.Background(),
-			query,
-			values...,
-		)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func assertDatabaseHasRows(t *testing.T, db *pgxpool.Pool, tableName string, expectedRecords []map[string]any) {
-	dbRows, err := db.Query(context.Background(), fmt.Sprintf("SELECT * FROM %s", tableName))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	results, err := pgx.CollectRows(dbRows, pgx.RowToMap)
-	if err != nil {
-		panic(err)
-	}
-
-	assert.Equal(t, len(expectedRecords), len(results), "Mismatched database rows count, expected %d, got %d", len(expectedRecords), len(results))
-
-	for _, expected := range expectedRecords {
-		for _, result := range results {
-
-			equalColumns := 0
-			for k := range expected {
-				if expected[k] == result[k] {
-					equalColumns++
-				}
-			}
-
-			assert.True(t, equalColumns == len(slices.Collect(maps.Keys(expected))), "Expected row not found in database: %v", expected)
-		}
 	}
 }
---
+++ internal/common/infra/postgresql/tests/helpers.go
@@ -0,0 +1,77 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"maps"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/assert"
+)
+
+func CleanUpTables(db *pgxpool.Pool, tables []string) {
+	for _, t := range tables {
+		db.Exec(context.Background(), fmt.Sprintf("DELETE FROM %s", t))
+	}
+}
+
+func WriteScenarioRows(db *pgxpool.Pool, tableName string, rows []map[string]any) {
+	for _, row := range rows {
+		var columns []string
+		var values []interface{}
+		var placeholders []string
+
+		for k, v := range row {
+			columns = append(columns, k)
+			placeholders = append(placeholders, fmt.Sprintf("$%d", len(values)+1))
+			values = append(values, v)
+		}
+
+		query := fmt.Sprintf(
+			"INSERT INTO %s (%s) VALUES (%s)",
+			tableName, strings.Join(columns, ", "), strings.Join(placeholders, ", "),
+		)
+
+		_, err := db.Exec(
+			context.Background(),
+			query,
+			values...,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func AssertDatabaseHasRows(t *testing.T, db *pgxpool.Pool, tableName string, expectedRecords []map[string]any) {
+	dbRows, err := db.Query(context.Background(), fmt.Sprintf("SELECT * FROM %s", tableName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results, err := pgx.CollectRows(dbRows, pgx.RowToMap)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, len(expectedRecords), len(results), "Mismatched database rows count, expected %d, got %d", len(expectedRecords), len(results))
+
+	for _, expected := range expectedRecords {
+		for _, result := range results {
+
+			equalColumns := 0
+			for k := range expected {
+				if expected[k] == result[k] {
+					equalColumns++
+				}
+			}
+
+			assert.True(t, equalColumns == len(slices.Collect(maps.Keys(expected))), "Expected row not found in database: %v", expected)
+		}
+	}
+}
