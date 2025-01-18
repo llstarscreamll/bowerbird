169f304918295b0fe770fb8f2cf31e4eedd5a3a1
Johan Alvarez <llstarscreamll@hotmail.com>
2025-01-18 13:34:36 -0500

    feat: new auth middleware

---
+++ internal/auth/infra/middlewares.go
@@ -0,0 +1,39 @@
+package infra
+
+import (
+	"context"
+	"fmt"
+	"llstarscreamll/bowerbird/internal/auth/domain"
+	"log"
+	"net/http"
+)
+
+func authMiddleware(next http.Handler, sessionRepo domain.SessionRepository, userRepo domain.UserRepository) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sessionIDCookie, err := r.Cookie("session_token")
+		if err != nil {
+			log.Printf("Error retrieving session_token cookie: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, `{"errors":[{"status":"400","title":"Bad request","detail":"Error retrieving session_token cookie"}]}`)
+			return
+		}
+
+		userID, err := sessionRepo.GetByID(r.Context(), sessionIDCookie.Value)
+		if err != nil {
+			log.Printf("Error getting session data from storage: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, `{"errors":[{"status":"500","title":"Internal server error","detail":"Error getting session data from storage"}]}`)
+			return
+		}
+
+		user, err := userRepo.GetByID(r.Context(), userID)
+		if err != nil {
+			log.Printf("Error getting user from storage: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), userContextKey, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
