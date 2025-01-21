0eb9194743dfd514dc98ca2f167c8c37ca508945
Johan Alvarez <llstarscreamll@hotmail.com>
2025-01-20 21:23:11 -0500

    feat: create mail_cretentials table on PostgreSQL schema

--- internal/common/infra/postgresql/migrations/20241230_init_schema.up.sql
+++ internal/common/infra/postgresql/migrations/20241230_init_schema.up.sql
@@ -15,3 +15,13 @@ CREATE TABLE IF NOT EXISTS "public"."sessions" (
     "expires_at" TIMESTAMP NOt NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS "public"."mail_credentials" (
+    "id" VARCHAR(26) PRIMARY KEY,
+    "user_id" VARCHAR(26) NOt NULL,
+    "mail_provider" VARCHAR(50) NOt NULL,
+    "access_token" VARCHAR(255) NOt NULL,
+    "refresh_token" VARCHAR(255) NOt NULL,
+    "expires_at" TIMESTAMP NOt NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
