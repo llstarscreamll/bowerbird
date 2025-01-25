38bb64cca14cbf59147e1f77dac2c6b3ecc874c9
Johan Alvarez <llstarscreamll@hotmail.com>
2025-01-25 07:46:58 -0500

    fix: change data type from some columns to support more long values

--- internal/common/infra/postgresql/migrations/20241230_init_schema.up.sql
+++ internal/common/infra/postgresql/migrations/20241230_init_schema.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "public"."users" (
+CREATE TABLE "public"."users" (
     "id" VARCHAR(26) PRIMARY KEY,
     "first_name" VARCHAR(255) NOT NULL,
     "last_name" VARCHAR(255) NOT NULL,
@@ -9,19 +9,19 @@ CREATE TABLE IF NOT EXISTS "public"."users" (
     unique("email")
 );
 
-CREATE TABLE IF NOT EXISTS "public"."sessions" (
+CREATE TABLE "public"."sessions" (
     "id" VARCHAR(26) PRIMARY KEY,
-    "user_id" VARCHAR(26) NOt NULL,
-    "expires_at" TIMESTAMP NOt NULL,
+    "user_id" VARCHAR(26) NOT NULL,
+    "expires_at" TIMESTAMP NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "public"."mail_credentials" (
+CREATE TABLE "public"."mail_credentials" (
     "id" VARCHAR(26) PRIMARY KEY,
-    "user_id" VARCHAR(26) NOt NULL,
-    "mail_provider" VARCHAR(50) NOt NULL,
-    "access_token" VARCHAR(255) NOt NULL,
-    "refresh_token" VARCHAR(255) NOt NULL,
-    "expires_at" TIMESTAMP NOt NULL,
+    "user_id" VARCHAR(26) NOT NULL,
+    "mail_provider" VARCHAR(50) NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "refresh_token" TEXT NOT NULL,
+    "expires_at" TIMESTAMP NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
