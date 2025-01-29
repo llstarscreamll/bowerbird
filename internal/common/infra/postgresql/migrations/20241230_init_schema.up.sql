ab344648ef61f11ac212cca894e5e0b9b36a354e
Johan Alvarez <llstarscreamll@hotmail.com>
2025-01-29 08:33:32 -0500

    feat: add unique constrain on PostgreSQL mail secret table by userID and email

--- internal/common/infra/postgresql/migrations/20241230_init_schema.up.sql
+++ internal/common/infra/postgresql/migrations/20241230_init_schema.up.sql
@@ -4,16 +4,17 @@ CREATE TABLE "public"."users" (
     "last_name" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "photo_url" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "last_login_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     unique("email")
 );
 
 CREATE TABLE "public"."sessions" (
     "id" VARCHAR(26) PRIMARY KEY,
     "user_id" VARCHAR(26) NOT NULL,
-    "expires_at" TIMESTAMP NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "expires_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "public"."mail_credentials" (
@@ -23,6 +24,8 @@ CREATE TABLE "public"."mail_credentials" (
     "mail_address" VARCHAR(255) NOT NULL,
     "access_token" TEXT NOT NULL,
     "refresh_token" TEXT NOT NULL,
-    "expires_at" TIMESTAMP NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "expires_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE UNIQUE INDEX "unique_mail_credentials_user_id_and_email" ON "public"."mail_credentials" ("user_id", "mail_address");
