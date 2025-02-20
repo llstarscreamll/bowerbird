59f26e9c40a2b37ea9a42a753414b09c0d6ced11
Johan Alvarez <llstarscreamll@hotmail.com>
2025-02-20 17:11:55 -0500

    feat: setup ngrx

--- static/web-app/src/app/app.config.ts
+++ static/web-app/src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideStore } from '@ngrx/store';
 import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
 import { routes } from './app.routes';
+import { reducers, metaReducers } from './reducers';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore(reducers, { metaReducers }),
+  ],
 };
