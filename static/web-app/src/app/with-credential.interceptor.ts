7464f402510516ab9d612a1c257f3e0fe56819b2
Johan Alvarez <llstarscreamll@hotmail.com>
2025-02-21 17:43:29 -0500

    style: format code

--- static/web-app/src/app/with-credential.interceptor.ts
+++ static/web-app/src/app/with-credential.interceptor.ts
@@ -1,18 +1,11 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpRequest,
-  HttpInterceptor,
-} from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class WithCredentialInterceptor implements HttpInterceptor {
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req.clone({ withCredentials: true }));
   }
 }
