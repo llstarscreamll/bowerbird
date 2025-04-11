d5e9e31a80fce0414937de2a1dde72b2f4023ffb
Johan Alvarez <llstarscreamll@hotmail.com>
2025-04-10 20:42:03 -0500

    feat: use route53 to bind custom domain to api gateway http api

--- cmd/aws-cdk/main.go
+++ cmd/aws-cdk/main.go
@@ -6,7 +6,10 @@ import (
 	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
 	apiGatewayV2 "github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2"
 	apiGatewayV2Integrations "github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2integrations"
+	certManager "github.com/aws/aws-cdk-go/awscdk/v2/awscertificatemanager"
 	lambda "github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	route53 "github.com/aws/aws-cdk-go/awscdk/v2/awsroute53"
+	route53Targets "github.com/aws/aws-cdk-go/awscdk/v2/awsroute53targets"
 	parameterStore "github.com/aws/aws-cdk-go/awscdk/v2/awsssm"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -27,7 +30,6 @@ func NewCdkStack(scope constructs.Construct, id string, props *CdkGoStackProps) 
 		ParameterName: jsii.String("prod-bowerbird-backend"),
 	})
 
-	// Create Lambda function
 	lambdaFn := lambda.NewFunction(stack, jsii.String("GoServer"), &lambda.FunctionProps{
 		FunctionName: jsii.String("API"),
 		Runtime:      lambda.Runtime_PROVIDED_AL2023(),
@@ -39,20 +41,40 @@ func NewCdkStack(scope constructs.Construct, id string, props *CdkGoStackProps) 
 		},
 	})
 
-	// Create HTTP API with Lambda integration
 	httpApi := apiGatewayV2.NewHttpApi(stack, jsii.String("HttpApi"), &apiGatewayV2.HttpApiProps{
 		ApiName:            jsii.String("BowerbirdApi"),
 		CreateDefaultStage: jsii.Bool(true),
 	})
 
-	// Create Lambda integration
+	hostedZone := route53.HostedZone_FromLookup(stack, jsii.String("HostedZone"), &route53.HostedZoneProviderProps{
+		DomainName: jsii.String("money-path.co"),
+	})
+
+	certificate := certManager.Certificate_FromCertificateArn(stack, jsii.String("AcmCertificate"), jsii.String("arn:aws:acm:us-east-1:336301087573:certificate/048e55de-9012-4c68-ad1e-6eb5d05478df"))
+
+	domainName := apiGatewayV2.NewDomainName(stack, jsii.String("ApiGatewayCustomDomain"), &apiGatewayV2.DomainNameProps{
+		DomainName:   jsii.String("money-path.co"),
+		EndpointType: apiGatewayV2.EndpointType_REGIONAL,
+		Certificate:  certificate,
+	})
+
+	apiGatewayV2.NewApiMapping(stack, jsii.String("ApiMapping"), &apiGatewayV2.ApiMappingProps{
+		Api:        httpApi,
+		DomainName: domainName,
+	})
+
+	route53.NewARecord(stack, jsii.String("ApiARecord"), &route53.ARecordProps{
+		Zone:       hostedZone,
+		Target:     route53.RecordTarget_FromAlias(route53Targets.NewApiGatewayv2DomainProperties(domainName.RegionalDomainName(), domainName.RegionalHostedZoneId())),
+		RecordName: jsii.String("api"),
+	})
+
 	integration := apiGatewayV2Integrations.NewHttpLambdaIntegration(
 		jsii.String("LambdaProxyIntegration"),
 		lambdaFn,
 		&apiGatewayV2Integrations.HttpLambdaIntegrationProps{},
 	)
 
-	// Add routes
 	httpApi.AddRoutes(&apiGatewayV2.AddRoutesOptions{
 		Path:        jsii.String("/{proxy+}"),
 		Methods:     &[]apiGatewayV2.HttpMethod{apiGatewayV2.HttpMethod_ANY},
@@ -65,7 +87,6 @@ func NewCdkStack(scope constructs.Construct, id string, props *CdkGoStackProps) 
 		Integration: integration,
 	})
 
-	// Grant Lambda permission to read from Parameter Store
 	secrets.GrantRead(lambdaFn)
 
 	return stack
