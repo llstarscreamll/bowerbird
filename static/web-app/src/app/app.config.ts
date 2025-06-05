5a0fcab6c589f9818cbb12cb9809e894064ac85d
Johan Alvarez <llstarscreamll@hotmail.com>
2025-06-04 22:10:00 -0500

    feat: set Angular and main index.html locale to es-CO

--- static/web-app/src/app/app.config.ts
+++ static/web-app/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from '@app/app.routes';
@@ -23,5 +23,6 @@ export const appConfig: ApplicationConfig = {
       useClass: WithCredentialInterceptor,
       multi: true,
     },
+    { provide: LOCALE_ID, useValue: 'es-CO' },
   ],
 };
--- static/web-app/src/main.ts
+++ static/web-app/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import localeEsCo from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsCo);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
